9bd00571ece40d11c900e364d7956680695f9182
Ender Bakır <enderyx_123@hotmail.com>
2024-03-12 22:13:25 +0300

    Added LeaveRequest.cs

--- src/Core/HRLeaveManagement.Domain/LeaveAllocation.cs
+++ src/Core/HRLeaveManagement.Domain/LeaveAllocation.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public int NumberOfDays { get; set; }
 
-        public LeaveType LeaveType { get; set; }
+        public LeaveType? LeaveType { get; set; }
 
         public int LeaveTypeId { get; set; }
 
