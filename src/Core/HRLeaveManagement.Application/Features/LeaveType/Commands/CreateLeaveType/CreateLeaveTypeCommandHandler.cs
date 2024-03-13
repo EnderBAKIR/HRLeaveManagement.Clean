efcf34fb36fbb0da7c8b30e2447711d89282a3bd
Ender Bakır <enderyx_123@hotmail.com>
2024-03-13 10:31:43 +0300

    Added CreateLeaveTypeCommand

---
+++ src/Core/HRLeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Contracts.Persistence;
+using HRLeaveManagement.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType
+{
+    public class CreateLeaveTypeCommandHandler : IRequestHandler<CreateLeaveTypeCommand, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public CreateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            _mapper = mapper;
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+
+        public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+
+            var leaveTpyeToCreate = _mapper.Map<Domain.LeaveType>(request);
+
+
+            await _leaveTypeRepository.CreateAsync(leaveTpyeToCreate);
+
+            return leaveTpyeToCreate.Id;
+        }
+    }
+}
