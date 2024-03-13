a5babb63680d49fbecb5294d53e6ec0820dd0ba9
Ender Bakır <enderyx_123@hotmail.com>
2024-03-13 19:14:02 +0300

    Completed LeaveRequestRepository

--- src/Core/HRLeaveManagement.Application/Contracts/Persistence/ILeaveRequestRepository.cs
+++ src/Core/HRLeaveManagement.Application/Contracts/Persistence/ILeaveRequestRepository.cs
@@ -4,9 +4,9 @@ namespace HRLeaveManagement.Application.Contracts.Persistence
 {
     public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
     {
-
-
-
+        Task<LeaveRequest> GetLeaveRequestWithDetails(int id);
+        Task<List<LeaveRequest>> GetLeaveRequestsWithDetails();
+        Task<List<LeaveRequest>> GetLeaveRequestsWithDetails(string userId);
     }
 
 }
--- src/Infrastructure/HRLeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
+++ src/Infrastructure/HRLeaveManagement.Persistence/Repositories/LeaveRequestRepository.cs
@@ -1,6 +1,7 @@
 ﻿using HRLeaveManagement.Application.Contracts.Persistence;
 using HRLeaveManagement.Domain;
 using HRLeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace HRLeaveManagement.Persistence.Repositories
 {
@@ -8,6 +9,26 @@ namespace HRLeaveManagement.Persistence.Repositories
     {
         public LeaveRequestRepository(HRDatabaseContext context) : base(context)
         {
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails()
+        {
+            var leaveRequests = await _context.LeaveRequests.Include(x => x.LeaveType).ToListAsync();
+
+            return leaveRequests;
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestsWithDetails(string userId)
+        {
+            var leaveRequests = await _context.LeaveRequests.Where(x => x.RequestingEmloyeeId == userId).Include(x => x.LeaveType).ToListAsync();
+            return leaveRequests;
+        }
+
+        public async Task<LeaveRequest> GetLeaveRequestWithDetails(int id)
+        {
+            var leaveRequest = await _context.LeaveRequests.Include(x => x.LeaveType).FirstOrDefaultAsync(x => x.Id == id);
+
+            return leaveRequest;
         }
     }
 }
