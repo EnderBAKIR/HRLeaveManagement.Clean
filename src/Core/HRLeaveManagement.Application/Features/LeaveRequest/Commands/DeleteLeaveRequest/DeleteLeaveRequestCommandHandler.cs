398f0ec3954a03e6868303c2df3c465f1b551fd8
Ender Bakır <enderyx_123@hotmail.com>
2024-03-14 16:50:07 +0300

    Added DeleteLeaveRequestCommand

---
+++ src/Core/HRLeaveManagement.Application/Features/LeaveRequest/Commands/DeleteLeaveRequest/DeleteLeaveRequestCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using HRLeaveManagement.Application.Contracts.Persistence;
+using HRLeaveManagement.Application.Exceptions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application.Features.LeaveRequest.Commands.DeleteLeaveRequest
+{
+    public class DeleteLeaveRequestCommandHandler : IRequestHandler<DeleteLeaveRequestCommand, Unit>
+    {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+
+        public DeleteLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            var leaveRequest = await _leaveRequestRepository.GetByIdAsync(request.Id);
+
+            if (leaveRequest == null)
+                throw new NotFoundException(nameof(LeaveRequest), request.Id);
+
+            await _leaveRequestRepository.DeleteAsync(leaveRequest);
+            return Unit.Value;
+        }
+    }
+}
