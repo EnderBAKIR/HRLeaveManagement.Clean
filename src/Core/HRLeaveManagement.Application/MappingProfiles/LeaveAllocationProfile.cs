dd4c4612ab63249c7b0e21272af7103c4e1f66fe
Ender Bakır <enderyx_123@hotmail.com>
2024-03-14 17:00:30 +0300

    Added LeaveAllocationProfile => /MappingProfiles

---
+++ src/Core/HRLeaveManagement.Application/MappingProfiles/LeaveAllocationProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Features.LeaveAllocation.Commands.CreateLeaveAllocation;
+using HRLeaveManagement.Application.Features.LeaveAllocation.Commands.UpdateLeaveAllocation;
+using HRLeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetails;
+using HRLeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocations;
+using HRLeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application.MappingProfiles
+{
+    public class LeaveAllocationProfile : Profile
+    {
+        public LeaveAllocationProfile()
+        {
+            CreateMap<LeaveAllocationDto, LeaveAllocation>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationDetailsDto>();
+            CreateMap<CreateLeaveAllocationCommand, LeaveAllocation>();
+            CreateMap<UpdateLeaveAllocationCommand, LeaveAllocation>();
+        }
+    }
+}
