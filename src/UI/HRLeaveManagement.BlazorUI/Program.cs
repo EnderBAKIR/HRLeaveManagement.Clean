6313e6baa9aa01c630d48b4ae77a940a93b08d82
Ender Bakır <enderyx_123@hotmail.com>
2024-03-15 14:48:12 +0300

    Added ILeaveTypeService And BaseHtppService

--- src/UI/HRLeaveManagement.BlazorUI/Program.cs
+++ src/UI/HRLeaveManagement.BlazorUI/Program.cs
@@ -1,10 +1,14 @@
 using HRLeaveManagement.BlazorUI.Components;
+using HRLeaveManagement.BlazorUI.Services.Base;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:7292"));
+
 
 var app = builder.Build();
 
